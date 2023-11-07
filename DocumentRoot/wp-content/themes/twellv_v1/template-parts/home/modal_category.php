ebd91590633e02c2be9e789c4c2f7b30281495f5
Le Huyen <lehuyen130901@gmail.com>
2023-11-07 11:27:10 +0700

    fix btn_watch and ui

--- DocumentRoot/wp-content/themes/twellv_v1/template-parts/home/modal_category.php
+++ DocumentRoot/wp-content/themes/twellv_v1/template-parts/home/modal_category.php
@@ -11,7 +11,10 @@ $modal_title = $args['title'];
             <div class="tlt">
                 <h2><?php echo $modal_title?></h2>
                 <div class="btn_watch">
-                    <a href="<?php echo esc_url(home_url('/howtowatch'))?>">無料で見られる！BS12の視聴方法</a>
+                    <a href="<?php echo esc_url(home_url('/howtowatch')) ?>">
+                        <span></span>
+                        <span>無料で見られる！BS12の視聴方法</span>
+                    </a>
                 </div>
             </div>
             <div class="list_watch">
