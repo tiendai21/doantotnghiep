1a68dea1d0598e6d21c07b2237be3d20f9bf85dc
Tung Lam <tunglam123ik54@gmail.com>
2023-11-13 17:09:53 +0700

    Update css program_schedule

--- DocumentRoot/wp-content/themes/twellv_v1/functions.php
+++ DocumentRoot/wp-content/themes/twellv_v1/functions.php
@@ -33,6 +33,8 @@ function theme_enqueue_styles()
 
     wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array(), $css_unti_cache);
 
+    wp_enqueue_script('program_schedule', get_stylesheet_directory_uri() . '/assets/js/program_schedule.js', array(), $css_unti_cache);
+
     if (is_archive("program")) {
         wp_enqueue_script('single-js', get_stylesheet_directory_uri() . '/assets/js/single.js', array(), $css_unti_cache);
     }
