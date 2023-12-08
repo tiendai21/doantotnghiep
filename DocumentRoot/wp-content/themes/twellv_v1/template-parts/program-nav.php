d0611afaf80574a0fff595c5eb6e98388cce5ea8
Le Huyen <lehuyen130901@gmail.com>
2023-12-08 18:08:35 +0700

    fix dbs 08-12

--- DocumentRoot/wp-content/themes/twellv_v1/template-parts/program-nav.php
+++ DocumentRoot/wp-content/themes/twellv_v1/template-parts/program-nav.php
@@ -2,8 +2,7 @@
 global $bs12_program_top_parts_arr;
 
 $nav = $bs12_program_top_parts_arr['nav'];
-// var_dump( $nav->ID );
-
+// var_dump( $nav );
 if (have_rows('navs', $nav->ID)) {
     ?>
     <ul class="items_link">
@@ -13,13 +12,11 @@ if (have_rows('navs', $nav->ID)) {
             $ttl = get_sub_field('ttl');
             $link = get_sub_field('link');
             $target_blank = get_sub_field('target_blank') ? ' target="_blank" ' : '';
-            if ($ttl !== "放送ラインアップ") :
-                ?>
-                <li>
-                    <a href="<?php echo esc_url($link); ?>" <?php echo $target_blank; ?>><?php echo esc_attr($ttl); ?></a>
-                </li>
-            <?php
-            endif;
+            ?>
+            <li>
+                <a href="<?php echo esc_url($link); ?>" <?php echo $target_blank; ?>><?php echo esc_attr($ttl); ?></a>
+            </li>
+        <?php
         endwhile;
         reset_rows();
         ?>
