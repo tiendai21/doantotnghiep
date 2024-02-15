ac019e8ca6c31ff5842069777ef35957b0abe721
trudc@caerux.com <trudc@caerux.com>
2024-02-15 14:17:41 +0700

    update Other 15/02

--- DocumentRoot/wp-content/themes/twellv_v1/archive-program.php
+++ DocumentRoot/wp-content/themes/twellv_v1/archive-program.php
@@ -139,14 +139,7 @@ if (!empty($term_query) && !is_wp_error($term_query)) {
         <?php get_template_part('template-parts/home/recommend_top', null, array('hideBrand' => false)); ?>
         <!-- /recommend -->
         <!-- other -->
-        <section class="section" id="other">
-            <div class="inner">
-                <div class="tlt_section">
-                    <h2>人気の番組カテゴリ</h2>
-                </div>
-                <?php get_template_part('template-parts/seo/category_famous_list'); ?>
-            </div>
-        </section>
+        <?php get_template_part('template-parts/single-pages/other', null, array('type' => 'all')); ?>
         <!-- /other -->
     </main>
     <!-- /main -->
