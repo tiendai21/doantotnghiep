21ce6b8c1384864d021e80b068d51c065e012ee1
hoangvv <hoangvv@caerux.com>
2024-02-22 17:03:24 +0700

    fix: banner empty

--- DocumentRoot/wp-content/themes/twellv_v1/inc/theme-setup.php
+++ DocumentRoot/wp-content/themes/twellv_v1/inc/theme-setup.php
@@ -553,34 +553,34 @@ function get_preview_id($postId) {
     return $previewId;
 }
 
-add_filter('get_post_metadata', function($meta_value, $post_id, $meta_key, $single) {
-    if ($preview_id = get_preview_id($post_id)) {
-        if ($post_id != $preview_id) {
-            $meta_value = get_post_meta($preview_id, $meta_key, $single);
-        }
-    }
-    return $meta_value;
-}, 10, 4);
+//add_filter('get_post_metadata', function($meta_value, $post_id, $meta_key, $single) {
+//    if ($preview_id = get_preview_id($post_id)) {
+//        if ($post_id != $preview_id) {
+//            $meta_value = get_post_meta($preview_id, $meta_key, $single);
+//        }
+//    }
+//    return $meta_value;
+//}, 10, 4);
 
-add_action('wp_insert_post', function ($postId) {
-    global $wpdb;
-    if (wp_is_post_revision($postId)) {
-        if (count($_POST['fields']) != 0) {
-            foreach ($_POST['fields'] as $key => $value) {
-                $field = get_field($key);
-                if ( !isset($field['name']) || !isset($field['key']) ) continue;
-                if (count(get_metadata('post', $postId, $field['name'], $value)) != 0) {
-                    update_metadata('post', $postId, $field['name'], $value);
-                    update_metadata('post', $postId, "_" . $field['name'], $field['key']);
-                } else {
-                    add_metadata('post', $postId, $field['name'], $value);
-                    add_metadata('post', $postId, "_" . $field['name'], $field['key']);
-                }
-            }
-        }
-        do_action('save_preview_postmeta', $postId);
-    }
-});
+//add_action('wp_insert_post', function ($postId) {
+//    global $wpdb;
+//    if (wp_is_post_revision($postId)) {
+//        if (count($_POST['fields']) != 0) {
+//            foreach ($_POST['fields'] as $key => $value) {
+//                $field = get_field($key);
+//                if ( !isset($field['name']) || !isset($field['key']) ) continue;
+//                if (count(get_metadata('post', $postId, $field['name'], $value)) != 0) {
+//                    update_metadata('post', $postId, $field['name'], $value);
+//                    update_metadata('post', $postId, "_" . $field['name'], $field['key']);
+//                } else {
+//                    add_metadata('post', $postId, $field['name'], $value);
+//                    add_metadata('post', $postId, "_" . $field['name'], $field['key']);
+//                }
+//            }
+//        }
+//        do_action('save_preview_postmeta', $postId);
+//    }
+//});
 
 /**
  * smartnews RSS
